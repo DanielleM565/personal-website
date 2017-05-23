da9a17480a7b0f4e0df0fc8350adbfd3a2754df5
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-22 23:44:17 -0600

    added bootstrap and google font styles started header

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">\
+<html lang="en">
 <head>
 	<title>Isles Designs</title>
+	<meta charset="UTF-8">
+	<!--For Internet explorer users please see http://lmgtfy.com/?q=how+to+download+google -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<!--Boot strap style pages from http://getbootstrap.com/getting-started/ -->
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+	<!-- Google fonts for header-->
+	<link href="https://fonts.googleapis.com/css?family=Lovers+Quarrel|Offside" rel="stylesheet">
+
+
 </head>
 	<body>
-		<p>Start of personal website page</p>
+		<header>
+			<div class="container">
+				<div class="row" style="background: lightgrey">
+					<!--First title in lovers quarrel google font-->
+					<div class="col-sm-1"><h2>Isles </h2></div>
+				</div> <!-- 1st row -->
+				<div class="row" style="background: darkgray">
+					<!-- second part of title should be offside google font-->
+					<div class="col-sm-2"><h2>Designs</h2></div>
+					</div> <!-- 2nd row -->
+			</div> <!--container -->
+		</header>
 	</body>
 </html>
