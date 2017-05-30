f55377e9f56f62fe2c972f1710ba82417b500ce6
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-29 22:41:25 -0600

    added a few more things to organize indents in index.php

--- public_html/index.php
+++ public_html/index.php
@@ -4,30 +4,30 @@
 	<title>Isles Designs</title>
 	<meta charset="UTF-8">
 	<!--For Internet explorer users please see http://lmgtfy.com/?q=how+to+download+google -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 	<!--Boot strap style pages from http://getbootstrap.com/getting-started/ -->
 
 	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 	<!--font awesome for icons and stuff-->
-	<link rel="stylesheet" href="https://maxcd.bootstraocdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://maxcd.bootstraocdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<!-- Google fonts for header and body-->
-	<link href="https://fonts.googleapis.com/css?family=Lovers+Quarrel|Offside" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Lovers+Quarrel|Offside" rel="stylesheet">
 
 	<!-- my custom css-->
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
 
 	<!--jQuery v3.0 -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 
 	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 </head>
 
@@ -40,15 +40,19 @@
 				<div class="row">
 					<div class="isles">
 					<!--First title in lovers quarrel google font-->
-					<div class="col-1"><h1>Isles </h1></div>
+						<div class="col-1">
+							<h1>Isles </h1>
+						</div>
 					</div>
 				</div> <!-- 1st row -->
 				<div class="row">
 					<div class="designs">
 						<!-- second part of title should be offside google font-->
-						<div class="col-2"><h1>Designs</h1></div>
+						<div class="col-2">
+							<h1>Designs</h1>
+						</div>
 					</div>
-					</div> <!-- 2nd row -->
+				</div> <!-- 2nd row -->
 			</div> <!--container -->
 		</header>
 
@@ -57,14 +61,14 @@
 			<div class="about">
 				<div class="row">
 					<div class="pull-left">
-					<h2>About me:</h2>
-						<p>Hello I am Danielle Isles Martin and I truly believe a good balance of art and science is what makes good design. As a Photographer, Web Developer and Scientist I live in that balance between science and art, constantly creating and testing new ideas. If you have and idea and need a website let's talk! (add link to contact me section here) </p>
+						<h2>About me:</h2>
+							<p>Hello I am Danielle Isles Martin and I truly believe a good balance of art and science is what makes good design. As a Photographer, Web Developer and Scientist I live in that balance between science and art, constantly creating and testing new ideas. If you have and idea and need a website let's talk! (add link to contact me section here) </p>
 		<!-- link to contact me section here -->
 					</div>
-					<div class="pull-right">
-					<img class="img-circle" src="imagespwp/profile-picture.jpg" alt="picture of Danielle">
-					</div>
-			</div> <!-- row -->
+						<div class="pull-right">
+							<img class="img-circle" src="imagespwp/profile-picture.jpg" alt="picture of Danielle">
+						</div>
+				</div> <!-- row -->
 			</div>
 		</div> <!-- container end about me-->
 
