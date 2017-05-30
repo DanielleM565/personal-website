d79f4170f887674030e03e184b54edb285dae702
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-29 18:34:56 -0600

    added a few more things to css having trouble making things work using bootstrap and css

--- public_html/index.php
+++ public_html/index.php
@@ -14,10 +14,10 @@
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-	<!--font awesome -->
+	<!--font awesome for icons and stuff-->
 	<link rel="stylesheet" href="https://maxcd.bootstraocdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-	<!-- Google fonts for header-->
+	<!-- Google fonts for header and body-->
 	<link href="https://fonts.googleapis.com/css?family=Lovers+Quarrel|Offside" rel="stylesheet">
 
 	<!-- my custom css-->
@@ -29,22 +29,25 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-
-
-
-
 </head>
+
+
 	<body class="sfooter">
+
 		<!-- header with title and nav bar -->
 		<header>
-			<div class="container-fluid" id="header">
-				<div class="row" style="background: #50CBFE">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="isles">
 					<!--First title in lovers quarrel google font-->
-					<div class="col-1"><h1 id="isles">Isles </h1></div>
+					<div class="col-1"><h1>Isles </h1></div>
+					</div>
 				</div> <!-- 1st row -->
-				<div class="row" style="background: #6a6c6f">
-					<!-- second part of title should be offside google font-->
-					<div class="col-2"><h1 id="designs">Designs</h1></div>
+				<div class="row">
+					<div class="designs">
+						<!-- second part of title should be offside google font-->
+						<div class="col-2"><h1>Designs</h1></div>
+					</div>
 					</div> <!-- 2nd row -->
 			</div> <!--container -->
 		</header>
@@ -52,18 +55,19 @@
 		<!-- about me section-->
 		<div class="container">
 			<div class="about">
-			<div class="row">
-				<div class="pull-left">
-				<h1>About me:</h1>
-				<p>Hello I am Danielle Isles Martin and I trulybelieve a good balance of art and science is what makes good design. As a Photographer, Web Developer and scientist. I live in that balance between science and art, constantly creating and testing new ideas. If you have and idea and need a website lets talk! (add link to contact me section here) </p>
+				<div class="row">
+					<div class="pull-left">
+					<h2>About me:</h2>
+						<p>Hello I am Danielle Isles Martin and I truly believe a good balance of art and science is what makes good design. As a Photographer, Web Developer and Scientist I live in that balance between science and art, constantly creating and testing new ideas. If you have and idea and need a website let's talk! (add link to contact me section here) </p>
 		<!-- link to contact me section here -->
-				</div>
-				<div class="pull-right">
-				<img class="img-circle" src="imagespwp/profile-picture.jpg" alt="picture of Danielle">
-				</div>
+					</div>
+					<div class="pull-right">
+					<img class="img-circle" src="imagespwp/profile-picture.jpg" alt="picture of Danielle">
+					</div>
 			</div> <!-- row -->
 			</div>
 		</div> <!-- container end about me-->
+
 
 	</body>
 
