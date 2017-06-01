9fd5b94f4f968ca4e49f5d954c13be5836269e9f
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-01 12:51:38 -0600

    made lots of changes with cmd-space added contact html, added pictures and some div tags for spacing

--- public_html/index.php
+++ public_html/index.php
@@ -15,7 +15,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 	<!--font awesome for icons and stuff-->
-		<link rel="stylesheet" href="https://maxcd.bootstraocdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<!-- Google fonts for header and body-->
 		<link href="https://fonts.googleapis.com/css?family=Lovers+Quarrel|Offside" rel="stylesheet">
@@ -23,27 +23,22 @@
 	<!-- my custom css-->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 
-	<!--jQuery v3.0 -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+	<!-- 1. Add latest jQuery and fancyBox files -->
+	<script src=“https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js“></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
 
 	<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-	<!-- 1. Add latest jQuery and fancyBox files -->
-	<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css" />
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
-
-	<!-- 2. Create links -->
-	<a data-fancybox="gallery" href="big_1.jpg"><img src="small_1.jpg"></a>
-	<a data-fancybox="gallery" href="big_2.jpg"><img src="small_2.jpg"></a>
-	<!-- documentation -->
 
 
 </head>
 
 
 	<body class="sfooter">
+
 
 		<!-- header with title and nav bar -->
 		<header>
@@ -52,15 +47,18 @@
 			</div> <!--container -->
 		</header>
 
+		<div class="container-fluid"> <!-- all the content containter -->
+
 		<!--Photography using fancybox3 -->
 		<section>
 			<div class="container">
-				<!-- <a href="lightning.jpg" data-fancybox="group" data-caption="lightning">
-					img src="thumbnail_1.jpg" alt="lightening" />
+
+				 <a data-fancybox="group" data-caption="lightning">
+					<img class="img-responsive center-block" src="images-fancybox3/lightning.jpg" alt="lightning" />
 				</a>
-				<a href="b.jpg" data-fancybox="group" data-caption="blood-moon">
-					img src="thumbnail_2.jpg" alt="blood moon" />
-				</a>-->
+				<a data-fancybox="group" data-caption="blood-moon">
+					<img class="img-responsive center-block" src="images-fancybox3/blood-moon.jpg" alt="blood moon" />
+				</a>
 			</div>
 		</section>
 
@@ -82,7 +80,69 @@
 
 		<!-- portfolio section -->
 
+<!-- contact me section-->
+		<section id="contact-form">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8 center-block">
+					<!--Begin Contact Form-->
+					<form id="contact-form" action="php/mailer.php" method="post">
+						<div class="form-group">
+							<label for="name">Name <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-user" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="email">Email <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+								</div>
+								<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="subject">Subject</label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="message">Message <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-comment" aria-hidden="true"></i>
+								</div>
+								<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+							</div>
+						</div>
 
+						<!-- reCAPTCHA -->
+						<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+					</form>
+
+
+				</div> <!-- column-->
+			</div> <!-- row -->
+			<!--empty area for form error/success output-->
+			<div class="row">
+				<div class="col-xs-12">
+					<div id="output-area"></div>
+				</div>
+			</div>
+		</section>
+
+		</div> <!-- all the content container-->
 	</body>
 
 </html>
