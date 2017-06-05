130a8801847dbebf82b5c83fab8a48037fbced26
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-05 16:58:43 -0600

    working on re-sizing everything

--- public_html/index.php
+++ public_html/index.php
@@ -48,21 +48,23 @@
 
 		<!-- header with title and nav bar -->
 	<header>
-			<div class="row">
-				<div class="logo">
-					<img class="img-responsive" src="imagespwp/logo.png" alt="Isles Designs logo">
-				</div>
-			</div> <!--row -->
+<!--			<div class="row">-->
+<!--				<div class="logo">-->
+<!--					<img class="img-responsive" src="imagespwp/logo.png" alt="Isles Designs logo">-->
+<!--				</div>-->
+<!--			</div> <!--row -->-->
 	</header>
 
-		<div class="container"> <!-- all the content container -->
+		<div class="container all-th-things"> <!-- all the content container -->
 
 			<!--Photography -->
-				<div class="img-responsive center-block" id="photos">
-					<a data-fancybox="group" data-caption="lightning">
+			<div class="container">
+				<div class="img-responsive center" id="photos">
+					<a>
 						<img class="center-block" src="images-fancybox3/lightning.jpg" alt="lightning"/>
 					</a>
 				</div>
+			</div>
 
 
 			<!-- about me section-->
@@ -73,10 +75,9 @@
 						<p>Hello I am Danielle Isles Martin and I truly believe a good balance of art and science is what
 							makes good design. As a Photographer, Web Developer and Scientist I live in that balance between
 							science and art, constantly creating and testing new ideas. If you have and idea and need a website
-							let's talk! (add link to contact me section here) </p>
-						<!-- link to contact me section here -->
+							let's talk!</p>
 					</div> <!-- column text -->
-					<div class="col-md-4">
+					<div class="col-md-4 text-center">
 						<img class="img-circle display-block" src="imagespwp/profile-picture.jpg" alt="picture of Danielle">
 					</div> <!-- column img -->
 				</div> <!-- row -->
@@ -85,18 +86,46 @@
 
 			<!-- portfolio section panel style  -->
 			<div class="container" id="portfolio">
-				<div class="col-md-4 text-center">
-					<h3>Portfolio 1</h3>
-						<p>Blah blah blah</p>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<h2>Portfolio</h2>
+
+					</div> <!-- column -->
+				</div> <!-- portfolio tile row -->
+
+				<div class="row">
+					<div class="col-md-6 text-center">
+						<h3>Pi-O-Mirror</h3>
+						<p>A Raspberry Pi powered Smart mirror build on the software side with PHP, mySQL, HTML, CSS, JavaScript and a little bit of Python</p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<h3>Game Plan</h3>
+						<p> Co-founder and Developer for Game Plan. An interactive web platform that helps entrepreneurs and investors view their business’s progress at-a-glance. Built a mock site using HTML, CSS and Bootstrap for Start-up </p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<h3>Angular Attack</h3>
+						<p>Developed a Random Number generator using Angular 3 for the first time in 48-hour for an internation Hackathon known as Angular Attack.</p>
+					</div>
+
+					<!-- image/ icon for website -->
+
+					<div class="col-md-6 text-center">
+						<h3>My Personal Site</h3>
+						<p>Well you're on it now! This site is my personal site to showcase my portfolie as a photographer and Web Designer. If you're interested in more of my work check out my git hub or drop me a line below and let's talk!</p>
+					</div>
+				</div> <!-- row -->
+
+				<!-- git hub hyperlink -->
+				<div class="row">
+					<div class="col-md-12 text-right">
+						<p>See my code at github<a href="https://github.com/DanielleM565"> <i
+									class="fa fa-github fa-2x"></i></a> </p>
+					</div>
 				</div>
-				<div class="col-md-4 text-center">
-					<h3>Portfolio 2</h3>
-						<p>Blah blah blah</p>
-				</div>
-				<div class="col-md-4 text-center">
-					<h3>Portfolio 3</h3>
-						<p>Blah blah blah</p>
-				</div>
+				<!-- serious though if you're in dev tool looking at my code go to my github-->
+
 			</div> <!-- container -->
 
 
@@ -163,7 +192,7 @@
 				</div> <!-- row -->
 			</div> <!-- container -->
 
-		</div> <!-- all the content container-->
+		</div> <!-- all the content container -->
 
 			<footer class="icon">
 				<div class="container text-center">
